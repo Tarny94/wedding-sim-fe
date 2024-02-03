f21eefb295c60e4f460e9bf63d90e337c083a130
Alexandru Tarnicer <102902000+Tarny94@users.noreply.github.com>
2024-02-03 12:01:35 +0200

    Update supplies.service.ts

--- src/app/Pages/Supply/supplies.service.ts
+++ src/app/Pages/Supply/supplies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 // @ts-ignore
-import {Supply} from "../../interface/wedding-interfaces";
+import {Supply} from "../../Interface/wedding-interfaces";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
